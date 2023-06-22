4e0eec60f627035151e2f0c87b959a18297e4d7a
Silvio Fanzon <silvio.fanzon@gmail.com>
2023-06-22 15:51:41 +0100

    Signed-off-by: Silvio Fanzon <silvio.fanzon@gmail.com>

---
+++ sections/.vdoc.py
@@ -0,0 +1,761 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+
+# Code for plotting gamma
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generating array t
+t = np.array([-3,-2,-1,0,1,2,3])
+
+# Computing array f
+f = t**2
+
+# Plotting the curve
+plt.plot(t,f)
+
+# Plotting dots
+plt.plot(t,f,"ko")
+
+# Showing the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+
+# Displaying output of np.linspace
+
+import numpy as np
+
+# Generates array t by dividing interval 
+# (-3,3) in 7 parts
+t = np.linspace(-3,3, 7)
+
+# Prints array t
+print("t =", t)
+#
+#
+#
+#
+#
+#| echo: true
+
+# Plotting gamma with finer step-size
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generates array t by dividing interval 
+# (-3,3) in 100 parts
+t = np.linspace(-3,3, 100)
+
+# Computes f
+f = t**2
+
+# Plotting
+plt.plot(t,f)
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: false
+#| fig-cap: "Fermat's spiral" 
+# Plotting gamma with finer step-size
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 50, 500)
+x = np.sqrt(t) * np.cos(t)
+y = np.sqrt(t) * np.sin(t)
+
+plt.plot(x,y)
+plt.show()
+#
+#
+#
+#
+#
+#
+#| echo: true
+#| fig-cap: "Adding a bit of style" 
+#| code-overflow: wrap
+
+# Adding some style
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Computing Spiral
+t = np.linspace(0, 50, 500)
+x = np.sqrt(t) * np.cos(t)
+y = np.sqrt(t) * np.sin(t)
+
+# Generating figure
+plt.figure(1, figsize = (4,4))
+
+# Plotting the Spiral with some options
+plt.plot(x, y, '--', color = 'deeppink', linewidth = 1.5, label = 'Spiral')
+
+# Adding grid
+plt.grid(True, color = 'lightgray')
+
+# Adding title
+plt.title("Fermat's spiral for t between 0 and 50")
+
+# Adding axes labels
+plt.xlabel("x-axis", fontsize = 15)
+plt.ylabel("y-axis", fontsize = 15)
+
+# Showing plot legend
+plt.legend()
+
+# Show the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+
+import numpy as np
+
+t = np.arange(0,1, 0.2)
+print("t =",t)
+```
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: false
+#| fig-cap: "The 5 x 5 grid corresponding to the matrix A"
+#| label: fig-grid-example
+ 
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_list = np.linspace(0, 4, 5)
+y_list = np.linspace(0, 4, 5)
+
+X, Y = np.meshgrid(x_list, y_list)
+
+plt.figure(figsize = (4,4))
+plt.plot(X,Y, 'k.')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+ 
+# Demonstrating np.meshgrid
+
+import numpy as np
+
+# Generating x and y coordinates
+xlist = np.linspace(0, 4, 5)
+ylist = np.linspace(0, 4, 5)
+
+# Generating grid X, Y
+X, Y = np.meshgrid(xlist, ylist)
+
+# Printing the matrices X and Y
+# np.array2string is only needed to align outputs
+print('X =', np.array2string(X, prefix='X= '))
+print('\n')  
+print('Y =', np.array2string(Y, prefix='Y= '))
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+#| fig-cap: "Plot of the curve defined by f=0" 
+
+# Plotting f=0
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generates coordinates and grid
+xlist = np.linspace(-1, 1, 5000)
+ylist = np.linspace(-1, 1, 5000)
+X, Y = np.meshgrid(xlist, ylist)
+
+# Computes f
+Z =((3*(X**2) - Y**2)**2)*(Y**2) - (X**2 + Y**2)**4 
+
+# Creates figure object
+plt.figure(figsize = (4,4))
+
+# Plots level set Z = 0
+plt.contour(X, Y, Z, [0])
+
+# Set axes labels
+plt.xlabel("x-axis", fontsize = 15)
+plt.ylabel("y-axis", fontsize = 15)
+
+# Shows plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+# Generates and plots empty 3D axes
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+# Creates figure object
+fig = plt.figure(figsize = (4,4))
+
+# Creates 3D axes object
+ax = plt.axes(projection = '3d')
+
+# Shows the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#| echo: true
+# Generates 3 x 2 empty 3D axes
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+# Creates container figure object
+fig = plt.figure(figsize = (6,8))
+
+# Creates 6 empty 3D axes objects
+ax1 = fig.add_subplot(3, 2, 1, projection = '3d')
+ax2 = fig.add_subplot(3, 2, 2, projection = '3d')
+ax3 = fig.add_subplot(3, 2, 3, projection = '3d')
+ax4 = fig.add_subplot(3, 2, 4, projection = '3d')
+ax5 = fig.add_subplot(3, 2, 5, projection = '3d')
+ax6 = fig.add_subplot(3, 2, 6, projection = '3d')
+
+# Shows the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Plotting 3D Helix
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+# Generates figure and 3D axes
+fig = plt.figure(figsize = (4,4))
+ax = plt.axes(projection = '3d')
+
+# Plots grid
+ax.grid(True)
+
+# Divides time interval (0,6pi) in 100 parts 
+t = np.linspace(0, 6*np.pi, 100)
+
+# Computes Helix
+x = np.cos(t) 
+y = np.sin(t)
+z = t
+
+# Plots Helix - We added some styling
+ax.plot3D(x, y, z, color = "deeppink", linewidth = 2)
+
+# Setting title for plot
+ax.set_title('3D Plot of Helix')
+
+# Setting axes labels
+ax.set_xlabel('x', labelpad = 20)
+ax.set_ylabel('y', labelpad = 20)
+ax.set_zlabel('z', labelpad = 20)
+
+# Shows the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Plotting 3D Helix
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+# Generates figure object
+fig = plt.figure(figsize = (4,4))
+
+# Generates 2 sets of 3D axes
+ax1 = fig.add_subplot(1, 2, 1, projection = '3d')
+ax2 = fig.add_subplot(1, 2, 2, projection = '3d')
+
+# We will not show a grid this time
+ax1.grid(False)
+ax2.grid(False)
+
+# Divides time interval (0,6pi) in 100 parts 
+t = np.linspace(0, 6*np.pi, 100)
+
+# Computes Helix
+x = np.cos(t) 
+y = np.sin(t)
+z = t
+
+# Plots Helix on both axes
+ax1.plot3D(x, y, z, color = "deeppink", linewidth = 1.5)
+ax2.plot3D(x, y, z, color = "deeppink", linewidth = 1.5)
+
+# Setting title for plots
+ax1.set_title('Helix from above')
+ax2.set_title('Helix from side')
+
+# Changing viewing angle of ax1
+# View from above has elev = 90 and azim = 0
+ax1.view_init(elev = 90, azim = 0)
+
+# Changing viewing angle of ax2
+# View from side has elev = 0 and azim = 0
+ax2.view_init(elev = 0, azim = 0)
+
+# Shows the plot
+plt.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Plotting Fermat's Spiral
+
+# Import libraries
+import numpy as np
+import plotly.graph_objects as go
+
+# Compute times grid by dividing (0,50) in 
+# 500 equal parts
+t = np.linspace(0, 50, 500)
+
+# Computes Fermat's Spiral
+x = np.sqrt(t) * np.cos(t)
+y = np.sqrt(t) * np.sin(t)
+
+# Create empty figure object and saves 
+# it in the variable "fig"
+fig = go.Figure()
+
+# Create the line plot object
+data = go.Scatter(x = x, y = y, mode = 'lines', name = 'gamma')
+
+# Add "data" plot to the figure "fig"
+fig.add_trace(data)
+
+# Here we start with the styling options
+# First we set a figure title
+fig.update_layout(title_text = "Plotting Fermat's Spiral with Plotly")
+
+# Adjust figure size
+fig.update_layout(autosize = False, width = 600, height = 600)
+
+# Change background canvas color
+fig.update_layout(paper_bgcolor = "snow")
+
+# Axes styling 
+yaxis=dict(
+        title_text="Y-axis Title",
+        ticktext=["Very long label", "long label", "3", "label"],
+        tickvals=[1, 2, 3, 4],
+        tickmode="array",
+        titlefont=dict(size=30),
+
+# Display the figure
+fig.show()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
